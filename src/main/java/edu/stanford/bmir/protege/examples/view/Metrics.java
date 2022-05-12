42aed203c83153ea555b636b00bc5159217bafcb
Shamistan Huseynov <shamistan.huseynov@abb-bank.az>
2022-05-12 23:22:35 +0400

    initial commit

--- src/main/java/edu/stanford/bmir/protege/examples/view/Metrics.java
+++ src/main/java/edu/stanford/bmir/protege/examples/view/Metrics.java
@@ -1,53 +1,52 @@
 package edu.stanford.bmir.protege.examples.view;
 
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 import org.protege.editor.owl.model.OWLModelManager;
 import org.protege.editor.owl.model.event.EventType;
 import org.protege.editor.owl.model.event.OWLModelManagerListener;
 
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.stream.Collectors;
+
 public class Metrics extends JPanel {
 
-    private JButton refreshButton = new JButton("Refresh");
+    private JButton showButton = new JButton("Show");
 
-    private JLabel textComponent = new JLabel();
+    private JLabel textComponent = new JLabel("It is the summary of the ontology");
 
     private OWLModelManager modelManager;
 
 
-    private ActionListener refreshAction = e -> recalculate();
-    
+    private ActionListener showAction = e -> showClasses();
+
     private OWLModelManagerListener modelListener = event -> {
         if (event.getType() == EventType.ACTIVE_ONTOLOGY_CHANGED) {
-            recalculate();
+            showClasses();
         }
     };
-    
+
     public Metrics(OWLModelManager modelManager) {
-    	this.modelManager = modelManager;
-        recalculate();
-        
+        this.modelManager = modelManager;
+        showClasses();
+
         modelManager.addListener(modelListener);
-        refreshButton.addActionListener(refreshAction);
-        
+        showButton.addActionListener(showAction);
+
         add(textComponent);
-        add(refreshButton);
+        add(showButton);
     }
-    
+
     public void dispose() {
         modelManager.removeListener(modelListener);
-        refreshButton.removeActionListener(refreshAction);
+        showButton.removeActionListener(showAction);
     }
-    
-    private void recalculate() {
-        int count = modelManager.getActiveOntology().getClassesInSignature().size();
-        if (count == 0) {
-            count = 1;  // owl:Thing is always there.
-        }
-        textComponent.setText("Total classes = " + count);
+
+    private void showClasses() {
+        textComponent.setText("Classes = " + modelManager.getActiveOntology()
+                .getClassesInSignature()
+                .stream().map(Object::toString).map(s -> {
+            String word = s.split("#")[1];
+            return word.substring(0, word.length() - 1);
+            }).collect(Collectors.toList()));
     }
 }
